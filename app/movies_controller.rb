b743db055d34bd8c61510bb692714d5d102cb3ca
Ray Alva <rayalva407@gmail.com>
2020-04-29 17:20:03 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -28,7 +28,7 @@ def can_be_created_in_a_block
   # title == "Home Alone"
   # release_date == 1990
   
-  Movie.create(title: "Home Alone", release_date: 1990)
+  Movie.create({title: "Home Alone", release_date: 1990})
 end
 
 def can_get_the_first_item_in_the_database
